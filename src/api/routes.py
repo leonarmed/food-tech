727578ef94d355955babad2b7e3638b080a52225
Leonardo Medina <leonarmed@gmail.com>
2023-02-05 19:21:03 +0000

    Login and register backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,35 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/create/user', methods = ['POST'])
+def create_user():
+    body = request.json
+    new_user = User.create(body)
+    if not isinstance(new_user, User):
+        return jsonify({
+            "message": new_user["message"],
+            "success": False
+        }), new_user["status"]
+    user = User.query.filter_by(email=new_user.email).one_or_none()
+    return jsonify({
+        "success": True,
+        "message": "User created successfully",
+        "data": user.serialize()
+    }), 201
+
+@api.route('/login', methods = ["POST"])
+def login():
+    body = request.json
+    valid_credentials = User.verify_credentials(email=body["email"], password=body["password"])
+    if not isinstance(valid_credentials, User):
+        return jsonify({
+            "message": valid_credentials["message"],
+            "success": False
+        }), valid_credentials["status"]
+    access_token = create_access_token(identity=valid_credentials.id)
+    return jsonify({
+        "message": "Successfully verification",
+        "success": True,
+        "data": {"token": access_token}
+    }), 200
